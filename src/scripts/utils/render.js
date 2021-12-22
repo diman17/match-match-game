b23fec474e98ddec684b4957744f4e7bb99afff1
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2021-12-22 21:39:27 +0300

    refactor: restructure js files

---
+++ src/scripts/utils/render.js
@@ -0,0 +1,31 @@
+export const createElement = (template) => {
+  const element = document.createElement('div');
+  element.innerHTML = template;
+  return element.firstChild;
+};
+
+export const renderPosition = {
+  APPEND: 'append',
+  PREPEND: 'prepend',
+  BEFORE: 'before',
+  AFTER: 'after',
+};
+
+export const render = (container, component, position = 'append') => {
+  switch (position) {
+    case 'append':
+      container.append(component.getElement());
+      break;
+    case 'prepend':
+      container.prepend(component.getElement());
+      break;
+    case 'before':
+      container.before(component.getElement());
+      break;
+    case 'after':
+      container.after(component.getElement());
+      break;
+    default:
+      throw new Error('Unknown render position');
+  }
+};
